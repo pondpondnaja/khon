94b971615cf117524f911dbe56479a1f702a7504
P_Petch <futayo110@gmail.com>
2019-09-30 11:56:19 +0700

    Fix AR bug 29/Sep/2019 16:50 .

--- app/src/main/java/com/example/khonapp/ARActivity.java
+++ app/src/main/java/com/example/khonapp/ARActivity.java
@@ -18,33 +18,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Plane;
+import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.assets.RenderableSource;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+
+import java.util.Collection;
 
 public class ARActivity extends AppCompatActivity {
     private static final String TAG = "loadModel";
     private static final double MIN_OPENGL_VERSION = 3.0;
 
     private ArFragment arFragment;
+    private boolean isModelPlace;
     private Context context;
     //private String path = "http://192.168.64.2/3D/";
     private String path = "http://mungmee.ddns.net/3D/";
     private String extension = ".glb";
     private String ASSET_3D = "";
     private String foldername = "";
-    /*private String path = "3DModel";
-    private String filename_model = "lion.glb";
-    private String filename_bin = "NOVELO_EARTH.bin";
 
-    FirebaseStorage firebaseStorage;
-    StorageReference storageRef;
-    StorageReference ref;
-    StorageReference ref_bin;
-
-    DownloadManager downloadManager;
-    */
     private BottomSheetBehavior mbottomSheetBehavior;
     private TextView mtextViewState;
 
@@ -100,10 +97,13 @@ public class ARActivity extends AppCompatActivity {
             //download();
 
             arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arfragment_model);
-            arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            //Tap to place model.
+            /*arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
                 Log.d(TAG, "onCreate: placemodel init");
                 placeModel(hitResult.createAnchor());
-            });
+            });*/
+            //Auto detection surface.
+            arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdate);
         }
 
         mbottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
@@ -139,42 +139,27 @@ public class ARActivity extends AppCompatActivity {
         });
     }
 
-    /*private void download() {
-        storageRef = firebaseStorage.getInstance().getReference();
+    private void onUpdate(FrameTime frameTime) {
+        if(isModelPlace){
+            return;
+        }
 
-        ref = storageRef.child(path+"/"+"lion_model"+"/"+filename_model);
-        ref_bin = storageRef.child(filename_bin);
-
-        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                Log.d(TAG, "onSuccess: Path_model : "+uri);
-                String url = uri.toString();
-                downloadFiles(ARActivity.this,filename_model,".glb",DIRECTORY_DOWNLOADS,url);
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        Collection<Plane> planes = frame.getUpdatedTrackables(Plane.class);
+        for(Plane plane : planes){
+            if(plane.getTrackingState() == TrackingState.TRACKING){
+                Anchor anchor = plane.createAnchor(plane.getCenterPose());
+                Toast.makeText(getApplicationContext(),"Surface Detected",Toast.LENGTH_SHORT).show();
+                placeModel(anchor);
+                break;
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-
-            }
-        });
+        }
     }
 
-    private void downloadFiles(Context context, String filename, String fileextension, String destinationDirectory, String url){
-        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        Uri uri = Uri.parse(url);
-        DownloadManager.Request request = new DownloadManager.Request(uri);
-
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalFilesDir(context,destinationDirectory,filename);
-        downloadManager.enqueue(request);
-
-        File file = new File(getExternalFilesDir(destinationDirectory).getPath());
-        ASSET_3D = file.toString()+"/"+filename_model;
-        Log.d(TAG, "onReceive: Success PATH : " + ASSET_3D);
-    }*/
 
     private void placeModel(Anchor anchor) {
+        isModelPlace = true;
+
         Log.d(TAG, "onCreate: Place Model From "+ASSET_3D);
         Toast.makeText(getApplicationContext(),"Fetching Model : "+foldername,Toast.LENGTH_SHORT).show();
         ModelRenderable
