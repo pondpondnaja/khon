9ac781c8e6235a54d5d5fe9a502b7cbfcc8e1785
P_Petch <futayo110@gmail.com>
2019-12-31 13:21:43 +0700

    Update calendar function get event information from Json file 31/Dec/2019 13:21.

--- app/src/main/java/com/example/khonapp/CalendarFragment.java
+++ app/src/main/java/com/example/khonapp/CalendarFragment.java
@@ -6,30 +6,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
 import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class CalendarFragment extends Fragment {
     private static final String TAG = "FC";
+    private static final String URL = "http://192.168.64.2/3D/calendar.php";
+    //private static final String URL = "https://utg-fansub.me/3D/news.php";
 
     ArrayList<EventDay> events = new ArrayList<EventDay>();
+    ArrayList<String> year_month_day = new ArrayList<>();
+    ArrayList<String> location = new ArrayList<>();
     ArrayList<String> title = new ArrayList<>();
     ArrayList<String> description = new ArrayList<>();
     ArrayList<String> years = new ArrayList<>();
     ArrayList<String> months = new ArrayList<>();
     ArrayList<String> days = new ArrayList<>();
 
-    TextView text_title, text_description;
-    CalendarView calendarView;
+    private TextView text_title, text_description, text_location, location_t;
+    private CalendarView calendarView;
 
     @Nullable
     @Override
@@ -37,62 +52,24 @@ public class CalendarFragment extends Fragment {
         final View view = inflater.inflate(R.layout.calendarfra_layout, container, false);
 
         MainActivity activity = (MainActivity) getActivity();
-        activity.settoolbarTitle("Even list");
+        activity.settoolbarTitle("Even Calendar");
 
         calendarView = view.findViewById(R.id.calendarView);
         text_title = view.findViewById(R.id.event_title);
         text_description = view.findViewById(R.id.event_description);
+        text_location = view.findViewById(R.id.location_r);
+        location_t = view.findViewById(R.id.location);
 
-        String[] arr = {"2019-11-16", "2019-11-23", "2019-11-28", "2019-11-31", "2020-0-1", "2020-0-2"};
-        String[] des = {"description 1", "description 2", "description 3", "description 4", "description 5", "description 6"};
-        String[] tit = {"Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6"};
+        //String[] arr = {"2019-11-16", "2019-11-23", "2019-11-28", "2019-11-31", "2020-0-1", "2020-0-2"};
+        //String[] des = {"description 1", "description 2", "description 3", "description 4", "description 5", "description 6"};
+        //String[] tit = {"Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6"};
 
-        if (savedInstanceState == null) {
-            boolean match = false;
-            int date = calendarView.getFirstSelectedDate().get(Calendar.DATE);
-            for (int i = 0; i < days.size(); i++) {
-                if (String.valueOf(date).equals(days.get(i))) {
-                    if (text_description.getVisibility() != View.VISIBLE) {
-                        text_description.setVisibility(View.VISIBLE);
-                    }
-                    text_title.setText(tit[i]);
-                    text_description.setText(description.get(i));
-                    match = true;
-                    Log.d(TAG, "onDayClick: Event Day position : " + i);
-                    Log.d(TAG, "onDayClick: Event Description  : " + description.get(i));
-                    break;
-                } else {
-                    match = false;
-                }
-            }
-            if (!match) {
-                text_title.setText("No Event");
-                text_description.setVisibility(View.GONE);
-            }
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            String[] item = arr[i].split("-");
+        /*for (int i = 0; i < title.size(); i++) {
+            String[] item = year_month_day.get(i).split("-");
             years.add(item[0]);
             months.add(item[1]);
             days.add(item[2]);
-            description.add(des[i]);
-        }
-
-        for (int j = 0; j < years.size(); j++) {
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Integer.valueOf(years.get(j)), Integer.valueOf(months.get(j)), Integer.valueOf(days.get(j)));
-            events.add(new MyEventDay(calendar, R.drawable.calendar_dot, tit[j]));
-        }
-        /*Calendar calendar1 = Calendar.getInstance();
-        calendar1.set(2019,11,4);
-        events.add(new MyEventDay(calendar1, R.drawable.circle_btn_after, "I am Event 1"));
-
-        Calendar calendar2 = Calendar.getInstance();
-        calendar2.set(2019,11,11);
-        events.add(new MyEventDay(calendar2, R.drawable.circle_btn_after, "I am Event 2"));*/
-
-        calendarView.setEvents(events);
+        }*/
 
         calendarView.setOnDayClickListener(new OnDayClickListener() {
             @Override
@@ -100,17 +77,27 @@ public class CalendarFragment extends Fragment {
                 //Log.d(TAG, "onDayClick: " + eventDay.getCalendar().get(Calendar.DATE));
                 int i;
                 boolean match = false;
+                String date = String.valueOf(eventDay.getCalendar().get(Calendar.DATE));
+                String month_r = String.valueOf(eventDay.getCalendar().get(Calendar.MONTH));
                 for (i = 0; i < days.size(); i++) {
-                    if (String.valueOf(eventDay.getCalendar().get(Calendar.DATE)).equals(days.get(i))) {
-                        if (text_description.getVisibility() != View.VISIBLE) {
-                            text_description.setVisibility(View.VISIBLE);
+                    if (month_r.equals(months.get(i))) {
+                        Log.d(TAG, "onDayClick: Month : " + month_r + " = " + months.get(i));
+                        Log.d(TAG, "onDayClick: Day   : " + date + " = " + days.get(i));
+                        if (date.equals(days.get(i))) {
+                            Log.d(TAG, "onDayClick: Called");
+                            if (text_description.getVisibility() != View.VISIBLE) {
+                                text_description.setVisibility(View.VISIBLE);
+                                location_t.setVisibility(View.VISIBLE);
+                                text_location.setVisibility(View.VISIBLE);
+                            }
+                            text_title.setText(((MyEventDay) eventDay).getNote());
+                            text_description.setText(description.get(i));
+                            text_location.setText(location.get(i));
+                            match = true;
+                            Log.d(TAG, "onDayClick: Event Day position : " + i);
+                            Log.d(TAG, "onDayClick: Event Description  : " + ((MyEventDay) eventDay).getNote());
+                            break;
                         }
-                        text_title.setText(((MyEventDay) eventDay).getNote());
-                        text_description.setText(description.get(i));
-                        match = true;
-                        Log.d(TAG, "onDayClick: Event Day position : " + i);
-                        Log.d(TAG, "onDayClick: Event Description  : " + ((MyEventDay) eventDay).getNote());
-                        break;
                     } else {
                         match = false;
                     }
@@ -118,13 +105,97 @@ public class CalendarFragment extends Fragment {
                 if (!match) {
                     text_title.setText("No Event");
                     text_description.setVisibility(View.GONE);
+                    text_location.setVisibility(View.GONE);
+                    location_t.setVisibility(View.GONE);
                 }
             }
         });
         return view;
     }
 
-    public void initData() {
+    @Override
+    public void onStart() {
+        initData();
+        super.onStart();
+    }
 
+    public void initData() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //Log.d(TAG, "onResponse: JSON respond : "+response);
+                        for (int i = 0; i < response.length(); i++) {                    // Parsing json
+                            try {
+                                JSONObject obj = response.getJSONObject(i);
+                                String title_a = obj.getString("title");
+                                String description_a = obj.getString("description");
+                                String event_date_a = obj.getString("event_date");
+                                String location_a = obj.getString("location");
+                                Log.d(TAG, "onResponse: Title : " + title + " Event Date : " + year_month_day);
+
+                                title.add(title_a);
+                                description.add(description_a);
+                                year_month_day.add(event_date_a);
+                                location.add(location_a);
+                                //mImageURL.add(image_url);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        initEvent();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: Error appear");
+                Toast.makeText(getContext(), "Please check your internet connection or go to contact us.", Toast.LENGTH_LONG).show();
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(request);
+    }
+
+    public void initEvent() {
+        for (int i = 0; i < year_month_day.size(); i++) {
+            String[] item = year_month_day.get(i).split("-");
+            years.add(item[0]);
+            months.add(String.valueOf(Integer.valueOf(item[1]) - 1));
+            int change = Integer.valueOf(item[2]);
+            days.add(String.valueOf(change));
+        }
+        for (int j = 0; j < years.size(); j++) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Integer.valueOf(years.get(j)), Integer.valueOf(months.get(j)), Integer.valueOf(days.get(j)));
+            events.add(new MyEventDay(calendar, R.drawable.calendar_dot, title.get(j)));
+        }
+        calendarView.setEvents(events);
+
+        boolean match = false;
+        int date = calendarView.getFirstSelectedDate().get(Calendar.DATE);
+        for (int i = 0; i < days.size(); i++) {
+            if (String.valueOf(date).equals(days.get(i))) {
+                if (text_description.getVisibility() != View.VISIBLE) {
+                    text_description.setVisibility(View.VISIBLE);
+                    location_t.setVisibility(View.VISIBLE);
+                    text_location.setVisibility(View.VISIBLE);
+                }
+                text_title.setText(title.get(i));
+                text_description.setText(description.get(i));
+                text_location.setText(location.get(i));
+                match = true;
+                break;
+            } else {
+                match = false;
+            }
+        }
+        if (!match) {
+            text_title.setText("No Event");
+            text_description.setVisibility(View.GONE);
+            location_t.setVisibility(View.GONE);
+            text_location.setVisibility(View.GONE);
+        }
     }
 }
