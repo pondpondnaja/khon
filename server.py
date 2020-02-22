48d2e356b08dfff144c67aa0b486b37e9533a2be
moneykeeper <bananasontop@gmail.com>
2020-02-22 07:48:05 +0700

    fix bug query exception in server.py

--- server.py
+++ server.py
@@ -44,7 +44,7 @@ def init():
             cur.execute("select info_desc from process where info_num = 1")
             text = cur.fetchall()
             cur.execute("select * from news order by id desc")
-        news = cur.fetchall()
+            news = cur.fetchall()
         except pymysql.Error as e:
             print("Error %d: %s" % (e.args[0], e.args[1]))     
         cur.close()
